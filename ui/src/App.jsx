c1fd56f88e14404764037c3773e3b4323ce3e0c7
DamonHayes <Hayes.Damon10@gmail.com>
2025-03-20 12:04:06 -0400

    basic files

---
+++ ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>The Brookettes</h1>
+      <p>Authors: Brooke Sharpenski, Harman Gidda, Kiersten Morrow, Andrew Stamps, Marques Johnson, Damon Hayes</p>
+    </>
+  )
+}
+
+export default App
